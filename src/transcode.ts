64c109921a08788b7bc367f49290e4a234c39b76
Andreas Stassivik <andreas@stassi.net>
2022-12-18 17:30:30 -0800

    transcode binary transcoder integration

--- src/transcode.ts
+++ src/transcode.ts
@@ -1,5 +1,7 @@
 import { Buffer } from 'node:buffer'
+import strictEquals from './logic/strictEquals.js'
 import toBinaryOctet from './octet/toBinary.js'
+import toDecimalOctet from './octet/toDecimal.js'
 import {
   decode as decodeNumber,
   encode as encodeNumber,
@@ -17,7 +19,9 @@ export type BinaryTranscoder = {
 }
 
 const HEX = 'hex',
-  LATIN_1 = 'latin1'
+  LATIN_1 = 'latin1',
+  BINARY = 'binary',
+  strictEqualsBinary: (x: string) => boolean = strictEquals(BINARY)
 
 export default function transcode(
   param:
@@ -29,22 +33,25 @@ export default function transcode(
   const paramIsArray = Array.isArray(param),
     paramIsNumber = typeof param === 'number',
     paramIsString = !paramIsNumber && 'encoding' in param && 'text' in param,
+    paramIsBinary = paramIsString && strictEqualsBinary(param.encoding),
     paramIsUInt8 = param instanceof Uint8Array,
     toUInt8Array = (): Uint8Array => {
-      return paramIsUInt8
-        ? param
-        : paramIsNumber
-        ? encodeNumber(param)
-        : Uint8Array.from(
-            paramIsArray ? param : Buffer.from(param.text, param.encoding)
-          )
+      // TODO: Declarative replacement via ternary expressions
+      if (paramIsUInt8) {
+        return param
+      } else if (paramIsNumber) {
+        return encodeNumber(param)
+      } else if (paramIsBinary) {
+        return encodeNumber(toDecimalOctet(param.text))
+      } else if (paramIsArray) {
+        return Uint8Array.from(param)
+      } else {
+        return Uint8Array.from(Buffer.from(param.text, param.encoding))
+      }
     },
     toNumber = (): number => decodeNumber(toUInt8Array()),
-    toString = (encoding: BinaryStringEncoding) => (): string =>
-      (paramIsString
-        ? Buffer.from(param.text, param.encoding)
-        : Buffer.from(paramIsNumber ? toUInt8Array() : param)
-      ).toString(encoding)
+    toString = (targetEncoding: BinaryStringEncoding) => (): string =>
+      Buffer.from(toUInt8Array()).toString(targetEncoding)
 
   return {
     toNumber,
@@ -52,13 +59,7 @@ export default function transcode(
     toHex: toString(HEX),
     toLatin1: toString(LATIN_1),
     toArray(): number[] {
-      return paramIsArray
-        ? param
-        : paramIsUInt8
-        ? [...param]
-        : paramIsNumber
-        ? [...encodeNumber(param)]
-        : [...Buffer.from(param.text, param.encoding)]
+      return [...toUInt8Array()]
     },
     toBinary(): string {
       return toBinaryOctet(toNumber())
